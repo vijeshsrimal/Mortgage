c01f53bc2be900aa7abe8e934c43802069bfaf9d
vijeshsrimal <89153426+vijeshsrimal@users.noreply.github.com>
2023-06-02 17:53:59 +0530

    Add files via upload

---
+++ MortgageFraudsDetectionModels/Models/Buyer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortgageFraudsDetectionModels.Models
+{
+    public class Buyer
+    {
+        public int BuyerId { get; set; }
+        public float BuyerSalary { get; set; }
+        public float MortgageValue { get; set; }
+        public int CibilScore { get; set; }
+    }
+}
---
+++ MortgageFraudsDetectionModels/Models/ValuationReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortgageFraudsDetectionModels.Models
+{
+    public class ValuationReport
+    {
+        public int BuyerId { get; set; }
+        public int PropertyId { get; set; }
+        public float MarketValue { get; set; }
+        public float PurchasingPower { get; set; }
+        public string LegalOpinion { get; set; }
+    }
+}
